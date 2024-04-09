c45edd81d2b90d7a32fedb3277fa992a08e219ba
Angie Campuzano Betancur <Angie.campuzanob@autonoma.edu.co>
2024-04-08 21:42:45 -0500

    Se crea el proyecto Hospital San José, este consta de 3 paquetes con sus respectivas clases.

---
+++ src/autonoma/hospitalsanjose/models/Empleado.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.hospitalsanjose.models;
+
+/**
+ *
+ * @author Angie Campuzano Betancur & Brayan Estivel Díaz
+ */
+public class Empleado {
+    
+}
