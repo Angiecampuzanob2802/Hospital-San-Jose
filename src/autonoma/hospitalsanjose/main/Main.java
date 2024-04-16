d8b774bdaea0a79c3de3b8618f3adc215c071985
Brayan Diaz <estivel.1995@gmail.com>
2024-04-15 22:06:48 -0500

    Se agrega la clase images se hace la capa grafica con el incnio de sesion y la ventana principal se agrega el atributo administrador a gerente y se crea la clase administrador para hacer el inicio de sesion con usuario y contraseña

--- src/autonoma/hospitalsanjose/main/Main.java
+++ src/autonoma/hospitalsanjose/main/Main.java
@@ -4,6 +4,11 @@
  */
 package autonoma.hospitalsanjose.main;
 
+import autonoma.hospitalsanjose.models.Administrador;
+import autonoma.hospitalsanjose.models.Hospital;
+import autonoma.hospitalsanjose.views.InicioHospital;
+import autonoma.hospitalsanjose.views.VentanaPrincipalHospital;
+
 /**
  *
  * @author Angie Campuzano Betancur & Brayan Estivel Díaz
@@ -12,4 +17,14 @@ package autonoma.hospitalsanjose.main;
  */
 public class Main {
     
+    public static void main(String[] args) {
+        
+        Hospital hospital = new Hospital("San jose St Bonaventure", "2425 Samaritan Dr, San Jose, CA 95124, Estados Unidos", "+1 408-559-2011", 1.000000, 1965, true, "37.25157313140872 -121.94667417922749", "Nomina", 1.0000000000000,gerente);
+        Administrador administrador = new Administrador("Aarong", "12345678", "Aaron Glassman", "84.941.564", 69, "Neurocirujano");
+        hospital.setAdministrador(administrador);
+        
+        VentanaPrincipalHospital ventana = new VentanaPrincipalHospital(hospital);
+        InicioHospital login = new InicioHospital(ventana, true, hospital, ventana);
+        login.setVisible(true);
+    }
 }
